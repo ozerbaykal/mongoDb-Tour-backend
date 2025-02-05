09aed198bd9424bb751ca6d0eed114fcb7bf11a8
ozerbaykal <baykalozer87@gmail.com>
2025-02-06 02:45:44 +0300

    refactor: update signUp response  with signToken and createSendToken functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,25 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
-//jwt token'i olulturup döndüren fonkisyon
+//jwt token'i olulturup döndüren fonksiyon
 const signToken = (user_id) => {
     //jwt token i oluştur
-    return jwt.sign({
-        id: user_id
-    },
+    return jwt.sign(
+        {
+            id: user_id,
+        },
         process.env.JWT_SECRET,
         { expiresIn: process.env.JWT_EXP }
-
-    )
+    );
+};
+//jwt tokeni oluşturup client'e gönderir
+const createSendToken = (user, code, res) => {
+    //tokeni oluştur
+    const token = signToken(user._id);
+    //client e cevap gönder
+    res.status(code).json({ message: "Oturum açıldı", token, user })
 
 }
-
 
 //kayıt Ol
 exports.signUp = async (req, res) => {
@@ -25,48 +31,32 @@ exports.signUp = async (req, res) => {
             email: req.body.email,
             password: req.body.password,
             passwordConfirm: req.body.passwordConfirm,
-
-
-        })
-        //token oluşutur 
-
-        const token = signToken(newUser._id)
-
-
-
-
-        res.status(201).json({ message: "Hesabınız oluşturuldu", user: newUser, token })
+        });
+        //jwt token oluşutur ve client a gönder
+        createSendToken(newUser, 201, res)
 
     } catch (error) {
-
-        res.status(500).json({ message: "Üzgünüz bir sorun oluştu", error: error.message })
-
+        res
+            .status(500)
+            .json({ message: "Üzgünüz bir sorun oluştu", error: error.message });
     }
-}
+};
 
 //giriş yap
 exports.login = async (req, res) => {
     try {
-
-        res.status(201).json({ message: "giriş yapıldı", })
-
+        res.status(201).json({ message: "giriş yapıldı" });
     } catch (error) {
-
-        res.status(500).json({ message: "Üzgünüz bir sorun oluştu" })
-
+        res.status(500).json({ message: "Üzgünüz bir sorun oluştu" });
     }
-}
+};
 
 //çıkış yap
 
 exports.logout = (req, res) => {
     try {
-
-        res.status(201).json({ message: "Çıkış yapıldı", })
-
+        res.status(201).json({ message: "Çıkış yapıldı" });
     } catch (error) {
-
-        res.status(500).json({ message: "Üzgünüz bir sorun oluştu" })
-
+        res.status(500).json({ message: "Üzgünüz bir sorun oluştu" });
     }
-}
+};
