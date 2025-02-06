df689c02f9c2fc4e2d6c385e50d085dcd9772ced
ozerbaykal <baykalozer87@gmail.com>
2025-02-06 14:52:09 +0300

    feat: Add login functionality with validation and JWT token generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
 
 //jwt token'i olulturup döndüren fonksiyon
 const signToken = (user_id) => {
@@ -17,9 +18,8 @@ const createSendToken = (user, code, res) => {
     //tokeni oluştur
     const token = signToken(user._id);
     //client e cevap gönder
-    res.status(code).json({ message: "Oturum açıldı", token, user })
-
-}
+    res.status(code).json({ message: "Oturum açıldı", token, user });
+};
 
 //kayıt Ol
 exports.signUp = async (req, res) => {
@@ -33,8 +33,7 @@ exports.signUp = async (req, res) => {
             passwordConfirm: req.body.passwordConfirm,
         });
         //jwt token oluşutur ve client a gönder
-        createSendToken(newUser, 201, res)
-
+        createSendToken(newUser, 201, res);
     } catch (error) {
         res
             .status(500)
@@ -45,7 +44,32 @@ exports.signUp = async (req, res) => {
 //giriş yap
 exports.login = async (req, res) => {
     try {
-        res.status(201).json({ message: "giriş yapıldı" });
+        const { email, password } = req.body
+        //1)email ve şifre geldimi kontrol et
+        if (!email || !password) {
+            return res.status(400).json({ message: "Lütfen mail ve şifre giriniz" })
+        }
+
+        //2)client 'tan gelen email ile kayıtlı kullanıcı var mı kontrol et
+        const user = await User.findOne({ email })
+
+        //2.1)kayıtlı kullanıcı yoksa hata fırlat
+        if (!user) {
+            return res.status(400).json({ message: "girdiğiniz mail kayıtlı kullancı bulunamadı" })
+        }
+
+        //3)client'tan gelen şifre ile veritabanında saklanan haslenmiş şifre ile eşleniyor mu kontrol et
+        const isValid = await bcrypt.compare(password, user.password)
+
+        //3.1) şifre yanlışssa hata fırlat
+        if (!isValid) {
+            return res.status(400).json({ message: "şifreniz  hatalı,lütfen geçerli bir şifre girin" })
+
+        }
+
+        //4) jwt token i oluşturup gönder
+        createSendToken(user, 200, res)
+
     } catch (error) {
         res.status(500).json({ message: "Üzgünüz bir sorun oluştu" });
     }
